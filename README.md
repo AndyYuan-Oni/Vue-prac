1391f4b68aed48cc2c1f495e1381fcf3ae1c40ae
AndyYuan-Oni <andyyuan2017@gmail.com>
2020-07-27 17:55:44 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Vue-prac
-For personal practise
+# test
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <h3>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{todo.title}}
+      <button @click="$emit('del-todo',todo.id)" class="del">x</button>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.is-complete {
+  text-decoration: line-through;
+}
+</style>
