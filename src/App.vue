9ea8b027c5d8bef7d22efadfa7a983ac142182eb
AndyYuan-Oni <andyyuan2017@gmail.com>
2020-07-28 11:42:50 +0800

    fin

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,18 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Header from "./components/layout/Header";
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
+import Header from './components/layout/Header'
 export default {
-  name: "App",
+  name: 'app',
   components: {
     Header,
-    Todos,
-    AddTodo
   },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      //this.todos = this.todos.filter(todo => todo.id !== id);
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(() => (this.todos = this.todos.filter(todo => todo.id !== id)))
-        .catch(err => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-      axios
-        .post("https://jsonplaceholder.typicode.com/todos", {
-          title,
-          completed
-        })
-        .then(res => (this.todos = [...this.todos, res.data]))
-        .catch(err => console.log(err));
-      // this.todos = [...this.todos, newTodo];
-    }
-  },
-  created() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then(res => (this.todos = res.data))
-      .catch(err => console.log(err));
-  }
-};
+}
 </script>
 
 <style>
@@ -59,18 +22,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-.btn {
-  display: inline-block;
-  border: nonne;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
+#nav {
+  padding: 30px;
 }
-.btn:hover {
-  background: #666;
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,13 +1,18 @@
 <template>
   <header class="header">
     <h1>Todolist</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: "Header"
-};
+  name: 'Header',
+}
 </script>
 
 <style scoped>
@@ -20,5 +25,6 @@ export default {
 .header a {
   color: #fff;
   padding-right: 5px;
+  text-decoration: none;
 }
 </style>
